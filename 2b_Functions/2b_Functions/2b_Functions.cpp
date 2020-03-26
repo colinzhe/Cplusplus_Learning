fb2ab8571fb10d843ac2c42b2797b165e71b268f
colinzhe <colinzhe@gmail.com>
2020-03-25 21:53:06 -0400

    Functions

--- 2b_Functions/2b_Functions/2b_Functions.cpp
+++ 2b_Functions/2b_Functions/2b_Functions.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+void odd(int x);
+void even(int x);
+int factorial(int x);
+
 int add(int a, int b)
 {
     int r = a + b;
@@ -16,9 +20,19 @@ void duplicate(int& a, int& b, int& c)
     c *= 2;
 }
 
+inline std::string concat(const std::string& a, const std::string& b)
+{
+    return a + b;
+}
+
 void printmsg(void)
 {
     std::cout << "This is a message." << std::endl;
+}
+
+int divide(int a, int b = 2)
+{
+    return a / b;
 }
 
 int main()
@@ -31,4 +45,36 @@ int main()
     z = 3;
     duplicate(x, y, z);
     std::cout << "x = " << x << ", y = " << y << ", z = " << z << std::endl;
+
+    std::string str1("Hello ");
+    std::string str2("there");
+    std::cout << concat(str1, str2) << std::endl;
+    std::cout << str1 << str2 << std::endl;
+
+    std::cout << divide(z) << std::endl;
+
+    odd(2);
+
+    int num = 9;
+    std::cout << num << "! = " << factorial(num) << std::endl;
+}
+
+void odd(int x)
+{
+    if ((x % 2) != 0) std::cout << "It is odd." << std::endl;
+    else even(x);
+}
+
+void even(int x)
+{
+    if ((x % 2) == 0) std::cout << "It is even." << std::endl;
+    else odd(x);
+}
+
+int factorial(int x)
+{
+    if (x > 1)
+        return (x * factorial(x - 1));
+    else
+        return 1;
 }
